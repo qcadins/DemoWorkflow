220b706efc2146938732482b3faf5e8991754cee
Jeremy Andreas (Adins) <jeremy.andreas@ad-ins.com>
2022-08-09 15:44:35 +0700

    AITATR3-89 Penambahan pemilihan job title saat login

---
+++ Object Repository/LoginR3BranchManagerSuperuser/NewLogin/select_JobTitle.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_JobTitle</name>
+   <tag></tag>
+   <elementGuidId>f986302b-98a5-45d5-acfa-5c5ba7bd04e4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>/html/body/div[2]/div[2]/div/mat-dialog-container/app-rolepick/div/div/form/div[1]/div[4]/lib-ucdropdownsearch/div/div[2]/div/select</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html/body/div[2]/div[2]/div/mat-dialog-container/app-rolepick/div/div/form/div[1]/div[4]/lib-ucdropdownsearch/div/div[2]/div/select</value>
+   </webElementProperties>
+</WebElementEntity>
