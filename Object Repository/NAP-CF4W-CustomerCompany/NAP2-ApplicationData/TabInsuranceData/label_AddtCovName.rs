678d7f6b2ee650fc0f11d1fa2635dbb883b0ca64
Jeremy Andreas (Adins) <jeremy.andreas@ad-ins.com>
2022-09-26 11:34:02 +0700

    AITATR3-151

---
+++ Object Repository/NAP-CF4W-CustomerCompany/NAP2-ApplicationData/TabInsuranceData/label_AddtCovName.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_AddtCovName</name>
+   <tag></tag>
+   <elementGuidId>8aaa4e3d-1f8b-4254-9937-6e4e2482bea6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;insuranceCoverage&quot;]/div[5]/table/tbody[1]/tr[3]/td[6]/div/div/label</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;insuranceCoverage&quot;]/div[5]/table/tbody[1]/tr[3]/td[6]/div/div/label</value>
+   </webElementProperties>
+</WebElementEntity>
