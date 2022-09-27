c92e4c6c90dacba861b03d4047ebddb42d8d3ee4
Jeremy Andreas (Adins) <jeremy.andreas@ad-ins.com>
2022-09-27 11:52:18 +0700

    AITATR3-148 Objek Company

---
+++ Object Repository/NAP-CF4W-CustomerCompany/NAP2-ApplicationData/TabTermConditionData/span_appNo.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_appNo</name>
+   <tag></tag>
+   <elementGuidId>81c2120a-1978-489c-b1d2-2cc082915846</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;NewApplication&quot;]/div/div[1]/span/div/div[2]/label/a/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;NewApplication&quot;]/div/div[1]/span/div/div[2]/label/a/span</value>
+   </webElementProperties>
+</WebElementEntity>
