a910863b22ab4f9ac775b1fa1c05f14f25b8119b
Jeremy Andreas (Adins) <jeremy.andreas@ad-ins.com>
2022-11-10 17:11:31 +0700

    AITATR3-208

---
+++ Object Repository/AppView/Referantor/Referantor Tab.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Referantor Tab</name>
+   <tag></tag>
+   <elementGuidId>59f56a40-ea8e-45b7-b63b-1026cbe3ca78</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;mat-tab-label-0-2&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;mat-tab-label-0-2&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
