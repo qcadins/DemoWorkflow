f7b3fcaf36592034d7c39a9f5050a0fd314b53bf
Jeremy Andreas (Adins) <jeremy.andreas@ad-ins.com>
2022-12-22 15:08:39 +0700

    270 & 271

---
+++ Object Repository/NAP/ReturnHandling/ReturnExecutionNotes.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ReturnExecutionNotes</name>
+   <tag></tag>
+   <elementGuidId>a7ee117e-89ec-420d-9ac5-7557c83613a7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;formReturnInfo&quot;]/form/div/div/div[4]/textarea</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;formReturnInfo&quot;]/form/div/div/div[4]/textarea</value>
+   </webElementProperties>
+</WebElementEntity>
