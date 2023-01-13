096da746f4ffcdff78489fcbd12c4bfadf8277b3
= <jeremy.andreas@ad-ins.com>
2023-01-13 14:34:47 +0700

    referantor objek

---
+++ Object Repository/NAP-CF4W-CustomerPersonal/NAP2-ApplicationData/TabReferantorData/td_ReferantorType.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>td_ReferantorType</name>
+   <tag></tag>
+   <elementGuidId>62d46c7c-0b79-4c9b-b5b0-0737b1754975</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;accessoriesData&quot;]/div[2]/table/tbody/tr/td[4]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;accessoriesData&quot;]/div[2]/table/tbody/tr/td[4]</value>
+   </webElementProperties>
+</WebElementEntity>
