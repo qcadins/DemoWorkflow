fdbee61832f77c46a4b5a77a1b018fc874864a09
= <jeremy.andreas@ad-ins.com>
2023-01-15 23:16:34 +0700

    AITATR3-293

--- Scripts/NAP-CF4W-CustomerCompany/CommissionReservedFund/TabCommissionData/Script1659082315669.groovy
+++ Scripts/NAP-CF4W-CustomerCompany/CommissionReservedFund/TabCommissionData/Script1659082315669.groovy
@@ -65,18 +65,23 @@ if(GlobalVariable.RoleCompany == "Testing"){
 	ArrayList<String> refundFrom = result.get("From")
 	ArrayList<String> refundAmt = result.get("Amt")
 	
+	Integer resultMA = CustomKeywords.'commissionReserveFundData.verifyIncomeInfo.verifyMaxAllocation'(sqlConnectionLOS,appNo)
+	
 	'Looping untuk set nilai awal 0 untuk total amount allocate commission dan verif income info berdasarkan rule file'
 	for (int i = 0; i < countIncomeInfo; i++) {
 		TotalAllocateCommissionAmt.add(0.00)
 	
 		if(i==countIncomeInfo-1){
-//			modifyObjectMaxAllocatedAmount = WebUI.modifyObjectProperty(findTestObject('Object Repository/NAP/CommissionReservedFund/TabCommissionData/label_MaxAllocatedAmtIncome'),'xpath','equals',"//*[@id='viewIncomeInfo']/div["+(i+1)+"]/div[2]/label",true)
+			modifyObjectMaxAllocatedAmount = WebUI.modifyObjectProperty(findTestObject('Object Repository/NAP/CommissionReservedFund/TabCommissionData/label_MaxAllocatedAmtIncome'),'xpath','equals',"//*[@id='viewIncomeInfo']/div["+(i+1)+"]/div[2]/label",true)
 //			
 //			modifyObjectRemainingAllocatedAmount = WebUI.modifyObjectProperty(findTestObject('Object Repository/NAP/CommissionReservedFund/TabCommissionData/label_RemainingAllocatedAmount'),'xpath','equals',"//*[@id='viewRemainIncomeInfo']/div["+(i+1)+"]/div[2]/label",true)
 //			
 //			'cek max allocated amount = remaining allocated amount before calculate'
 //			checkVerifyEqualOrMatch(WebUI.verifyMatch(WebUI.getText(modifyObjectMaxAllocatedAmount),WebUI.getText(modifyObjectRemainingAllocatedAmount),false),'12.TabCommissionData',
 //				GlobalVariable.NumofColm)
+			
+			checkVerifyEqualOrMatch(WebUI.verifyMatch(WebUI.getText(modifyObjectMaxAllocatedAmount).replace(".00",""),resultMA.toString(),false),'12.TabCommissionData',
+				GlobalVariable.NumofColm)
 			
 			break
 		}
